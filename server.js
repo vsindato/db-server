8318cb12f75de9964fc6d9c9b0d7954b8330b826
victor sindato <vsindato@gmail.com>
2024-06-05 13:42:02 -0400

    setup server for in-memory stores and reads of key/value pairs

---
+++ server.js
@@ -0,0 +1,50 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 4000;
+
+// Middleware to parse JSON
+app.use(bodyParser.json());
+
+// Welcome route
+app.get('/', (req, res) => {
+    res.send('hello world!');
+});
+
+// In-memory data-store
+const dataStore = {};
+
+// Store key-value pair in in-memory store
+app.get('/set', (req, res) => {
+    const somekey = Object.keys(req.query)[0];
+    const somevalue = req.query[somekey];
+    if (somekey && somevalue) {
+        dataStore[somekey] = somevalue;
+        res.send(`The value stored at ${somekey} = ${somevalue}`);
+    } else {
+        res.status(400).send('somekey or somevalue was not provided');
+    }
+});
+
+
+// Get the stored value for a key
+app.get('/get', (req, res) => {
+    const { key } = req.query;
+    if (key) {
+        const value = dataStore[key];
+        if (value) {
+            res.send(`Value for ${key} is ${value}`);
+        } else {
+            res.status(404).send(`Key ${key} not found`);
+        }
+    } else {
+        res.status(400).send('Missing key');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
