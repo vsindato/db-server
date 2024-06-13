7c89c7fc2cda4f42c7dad7f0d10d4b1535806f75
victor sindato <vsindato@gmail.com>
2024-06-13 10:17:38 -0400

    convert from commonjs to esm

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js
-const express = require('express');
-const bodyParser = require('body-parser');
+import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 4000;
